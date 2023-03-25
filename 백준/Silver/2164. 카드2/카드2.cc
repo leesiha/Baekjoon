eb2cc1cf8f3272010693d4435f06efca666dc955
이시하 Lee Siha <66323295+leesiha@users.noreply.github.com>
2023-03-26 04:02:34 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4040 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+queue<int> q;
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+        q.push(i);
+    while (q.size() != 1)
+    {
+        q.pop();
+        q.push(q.front());
+        q.pop();
+    }
+    cout << q.front();
+}
