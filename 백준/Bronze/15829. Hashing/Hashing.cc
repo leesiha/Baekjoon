d8c95104d8210e11e1ca9ad98d25438eaa2a92d3
Siha Lee <mlice1030@soongsil.ac.kr>
2023-06-28 01:39:01 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <cmath>
+#define M 1234567891
 using namespace std;
-unsigned long L, r = 1, sum;
+size_t L, sum;
 string s;
 
 size_t pow_(size_t a, size_t b)
@@ -16,10 +17,6 @@ int main()
 {
     cin >> L >> s;
     for (size_t i = 0; i < L; i++)
-    {
-        for (size_t j = 0; j <= i; j++)
-            r *= 31;
-        sum += (char(s[i]) - 96) * pow_(31, i);
-    }
-    cout << sum % 1234567891;
+        sum = (sum + (char(s[i]) - 96) * pow_(31, i)) % M;
+    cout << sum;
 }
