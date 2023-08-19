200f8108a140b0a6fd99299e7b6d1c6b66b16b02
Siha Lee <mlice1030@soongsil.ac.kr>
2023-08-20 01:38:49 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,16 @@
+// 참고 :
+// 풀이 방법 - 수학
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int R1, S;
+    cin >> R1 >> S;
+
+    cout << 2*S - R1;
+}
