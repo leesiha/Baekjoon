207b052803b477ba2fda56427a5444710d4a8d6f
Siha Lee <mlice1030@soongsil.ac.kr>
2024-03-14 13:35:46 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,50 @@
+// 참고 : https://donggoolosori.github.io/2020/10/13/boj-11660/
+// DFS
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+#define MAX 9
+vector<int> v;
+int arr[MAX];
+bool visited[MAX];
+int N, M;
+
+void dfs(int idx, int cnt)
+{
+    if (cnt == M)
+    {
+        for (int i = 0; i != M; i++)
+            cout << arr[i] << ' ';
+        cout << "\n";
+        return;
+    }
+    for (int i = idx; i < N; i++)
+    {
+        if (!visited[i])
+        {
+            visited[i] = true;
+            arr[cnt] = v[i];
+            dfs(i+1, cnt+1);
+            visited[i] = false;
+        }
+    }   
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> N >> M;
+    for (int i = 0; i < N; i++)
+    {
+        int num;
+        cin >> num;
+        v.push_back(num);
+    }
+    sort(v.begin(), v.end());
+    dfs(0, 0);
+}
