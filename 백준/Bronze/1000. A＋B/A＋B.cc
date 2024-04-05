0df5b49a1a3833141a20093b818e47047fe05a22
Siha Lee <mlice1030@soongsil.ac.kr>
2024-04-05 23:21:14 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.cc
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -1,7 +1,10 @@
-#include <stdio.h>
-int main() {
-    int a,b;
-    scanf("%d %d",&a,&b);
-    printf("%d\n",a+b);
-    return 0;
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << a+b << '\n';
 }
