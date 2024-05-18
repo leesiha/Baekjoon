6d6ed9d4fbc14af50eb3e6d8bc50c6ea5ab7fe04
Siha Lee <mlice1030@soongsil.ac.kr>
2024-05-18 15:49:58 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,31 @@
+/*
+1 : SK
+2 : CY
+3 : SK
+4 : CY
+5 : SK
+6 : CY
+7 : SK
+8 : CY
+9 : SK
+10 : CY
+11 : SK
+12 :
+13 :
+14 :
+15 :
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    if (N % 2)
+        cout << "SK";
+    else
+        cout << "CY";
+}
